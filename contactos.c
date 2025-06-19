d1d36a37b2bf0c7313aecaf78f6e921923d78bf1
Santi Echaniz <sechaniz@etrr.edu.ar>
2025-06-18 23:23:19 -0300

    Create contactos.c
    pasado de gbd q lo venía haciendo

---
+++ contactos.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define agrecontacto 1
+#define mostrcontactos 2
+#define buscarcontacto 3
+#define elimcontacto 4
+#define guardar 5
+#define salir 6
+
+
+
+#define MAX 100
+
+typedef struct contacto {
+    char nombre[MAX];
+    char telefono[MAX];
+    char email[MAX];
+    char nota[MAX];
+    struct contacto *sig;
+} contacto_t;
+
+
+int main()
+{
+     do {
+        menu();
+        printf("Elegí una opcion: ");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case agrecontacto:
+              agenda = agregar_contacto(agenda);
+            break;
+  
+            case mostrcontactos:
+              mostrar_contactos(agenda);
+            break;
+
+            case buscarcontacto:
+              buscar_contacto(agenda);
+            break;
+
+            case elimcontacto:
+              agenda = eliminar_contacto(agenda);
+            break;
+
+            case guardar:
+              guardar_archivo(agenda);
+            break;
+
+            case salir:
+              guardar_archivo(agenda);
+              liberar_memoria(agenda);
+              printf("chau\n");
+            break;
+
+            default:
+              printf("Opción inválida\n");
+            break;
+        }
+
+    } while (opcion != 6);
+
+    return 0;
+    return 0;
+}
