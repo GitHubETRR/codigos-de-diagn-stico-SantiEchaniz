07e717f2f9501c282f2f0d3d1ce04d48ef74634c
Santi Echaniz <sechaniz@etrr.edu.ar>
2025-06-26 00:15:38 -0300

    Update contactos.c
    agregar contactos, buscarlos, e imprimirlos

--- contactos.c
+++ contactos.c
@@ -11,7 +11,7 @@
 
 typedef struct contacto {
     char nombre[MAX];
-    char telefono[MAX];
+    char numero[MAX];
     char email[MAX];
     char notas[MAX];
     struct contacto *sig;
@@ -63,6 +63,9 @@ int main()
 }
 
 
+
+
+
 ingreso(){
     int opcion;
     printf("\nMenu\n");
@@ -74,4 +77,92 @@ ingreso(){
     printf("           Opcion: ");
     scanf("%d", &opcion);
     return opcion;
+}
+
+
+
+
+contacto* agregar_contacto(contacto* agenda) {
+    contacto* nuevo = malloc(sizeof(contacto));
+    if (!nuevo) {
+        printf("no se pudo conseguir memoria\n");
+        return agenda;
+    }
+
+    printf("Nombre:");
+    fgets(nuevo->nombre, MAX, stdin);
+    nuevo->nombre[strcspn(nuevo->nombre, "\n")] = 0;
+
+    printf("Numero:");
+    fgets(nuevo->numero, MAX, stdin);
+    nuevo->numero[strcspn(nuevo->numero, "\n")] = 0;
+
+    nuevo->sig = agenda;
+    return nuevo;
+}
+
+
+
+
+void imprimir(contacto* agenda) {
+    if (!agenda) {
+        printf("está vacio\n");
+        return;
+    }
+
+    printf("\nContactos:\n");
+    while (agenda) {
+        printf("Nombre: %s\n", agenda->nombre);
+        printf("Numero: %s\n\n", agenda->numero);
+        agenda = agenda->sig;
+    }
+}
+
+
+
+
+
+void buscar(contacto* agenda) {
+    char nombre[MAX];
+    printf("Nombre a buscar: ");
+    fgets(nombre, MAX, stdin);
+    nombre[strcspn(nombre, "\n")] = 0;
+
+    while (agenda) {
+        if (strcmp(agenda->nombre, nombre) == 0) {
+            printf("Contacto:\nNombre: %s\nNumero: %s\n", agenda->nombre, agenda->numero);
+            return;
+        }
+        agenda = agenda->sig;
+    }
+
+    printf("No se encontro el contacto\n");
+}
+
+
+
+
+void guardar_en_archivo(contacto* agenda) {
+    FILE *archivo = fopen("C:\\Users\\sechaniz\\Desktop\\OTROS\\ContactosSantu.txt", "w");
+    if (!archivo) {
+        printf("No se pudo guardar el archivo\n");
+        return;
+    }
+
+    while (agenda) {
+        fprintf(archivo, "%s\n%s\n", agenda->nombre, agenda->numero);
+        agenda = agenda->sig;
+    }
+
+    fclose(archivo);
+    printf("Guardado'\n");
+}
+
+void liberar_memoria(contacto* agenda) {
+    contacto* aux;
+    while (agenda) {
+        aux = agenda;
+        agenda = agenda->sig;
+        free(aux);
+    }
 }
