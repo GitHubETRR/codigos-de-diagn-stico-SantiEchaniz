9fb0c72bcee07f092da40c20516f1dfafade012d
SantiEchaniz <sechaniz@etrr.edu.ar>
2025-07-03 10:19:01 -0300

    hola beatriz

---
+++ CORRECTO/fds.c
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 100
+
+typedef enum{
+    AGREGAR=1,
+    MOSTRAR,
+    BUSCAR,
+    ELIMINAR,
+    SALIR,
+}opciones_t;
+
+
+
+typedef struct contacto {
+    char nombre[MAX];
+    char numero[MAX];
+    char mail[MAX];
+    char notas[MAX];
+    struct contacto *sig;
+} contacto;
+
+int ingreso();
+contacto* cargar_anterior();
+contacto* agregar_contacto(contacto* agenda);
+void mostrar(contacto* agenda);
+void buscar(contacto* agenda);
+contacto* eliminar(contacto* agenda);
+void guardartodo(contacto* agenda);
+void liberar_memoria(contacto* agenda);
+
+
+int main() {
+    int opcion;
+    contacto *agenda = cargar_anterior(); //la actualizo con lo viejo
+    do {
+        opcion = ingreso();
+        switch (opcion) {
+            case AGREGAR:
+                agenda = agregar_contacto(agenda);
+                break;
+                
+            case MOSTRAR:
+                mostrar(agenda);
+                break;
+                
+            case BUSCAR:
+                buscar(agenda);
+                break;
+                
+            case ELIMINAR:
+                agenda = eliminar(agenda);
+                break;
+                
+            case SALIR:
+                guardartodo(agenda);
+                liberar_memoria(agenda);
+                printf("chau\n");
+                break;
+                
+            default:
+                printf("Opcion invalida\n");
+        }
+    } while (opcion != SALIR);
+    
+    return 0;
+}
+
+int ingreso() {
+    int opcion;
+    printf("\nMenu\n");
+    printf("1-Agregar contacto\n");
+    printf("2-Mostrar contactos\n");
+    printf("3-Buscar contacto\n");
+    printf("4-Eliminar contacto\n");
+    printf("5-Salir\n");
+    printf("\nOpcion: ");
+    scanf("%d", &opcion);
+    while (getchar() != '\n'); // limpio
+    return opcion;
+}
+
+contacto* agregar_contacto(contacto* agenda) {
+    contacto* nuevo = malloc(sizeof(contacto));
+    if (!nuevo) {
+        printf("No se pudo conseguir memoria\n");
+        return agenda;
+    }
+    printf("Nombre: ");
+    fgets(nuevo->nombre, MAX, stdin);
+    nuevo->nombre[strcspn(nuevo->nombre, "\n")] = 0;
+
+    printf("Numero: ");
+    fgets(nuevo->numero, MAX, stdin);
+    nuevo->numero[strcspn(nuevo->numero, "\n")] = 0;
+
+    printf("Mail: ");
+    fgets(nuevo->mail, MAX, stdin);
+    nuevo->mail[strcspn(nuevo->mail, "\n")] = 0;
+
+    printf("Notas: ");
+    fgets(nuevo->notas, MAX, stdin);
+    nuevo->notas[strcspn(nuevo->notas, "\n")] = 0;
+
+    nuevo->sig = agenda;
+    return nuevo;
+}
+
+void mostrar(contacto* agenda) {
+    if (!agenda) {
+        printf("Vacio\n");
+        return;
+    }
+    printf("\nContactos:\n");
+    while (agenda) {
+        int i=1;
+        printf("Contacto %d:", i);
+        printf("Nombre: %s\n", agenda->nombre);
+        printf("Numero: %s\n", agenda->numero);
+        printf("Email: %s\n", agenda->mail);
+        printf("Notas: %s\n", agenda->notas);
+        printf("--------------------------------------------------------\n");
+        agenda = agenda->sig;
+        i++;
+    }
+}
+
+void buscar(contacto* agenda) {
+    char nombre[MAX];
+    printf("Nombre a buscar: ");
+    fgets(nombre, MAX, stdin);
+    nombre[strcspn(nombre, "\n")] = 0;
+    while (agenda) {
+        if (strcmp(agenda->nombre, nombre) == 0) {
+            printf("Contacto encontrado:\n");
+            printf("Nombre: %s\n", agenda->nombre);
+            printf("Numero: %s\n", agenda->numero);
+            printf("Email: %s\n", agenda->mail);
+            printf("Notas: %s\n", agenda->notas);
+            return;
+        }
+        agenda = agenda->sig;
+    }
+    printf("El contacto no se encontro\n");
+}
+
+contacto* eliminar(contacto* agenda) {
+    char nombre[MAX];
+    printf("A quien quiero eliminar: ");
+    fgets(nombre, MAX, stdin);
+    nombre[strcspn(nombre, "\n")] = 0;
+
+    contacto *actual = agenda;
+    contacto *anterior = NULL;
+
+    while (actual) {
+        if (strcmp(actual->nombre, nombre) == 0) {
+            if (anterior) {
+                anterior->sig = actual->sig;
+            } else {
+                agenda = actual->sig;
+            }
+            free(actual);
+            printf("Contacto eliminado\n");
+            return agenda;
+        }
+        anterior = actual;
+        actual = actual->sig;
+    }
+
+    printf("Contacto no existe\n");
+    return agenda;
+}
+
+void guardartodo(contacto* agenda) {
+    FILE *archivo = fopen("C:\\Users\\sechaniz.ETRR\\Desktop\\OTROS\\ContactosSantu.txt", "w");
+    if (!archivo) {
+        printf("No se pudo guardar el archivo\n");
+        return;
+    }
+    while (agenda) {
+        fprintf(archivo, "%s\n%s\n%s\n%s\n", agenda->nombre, agenda->numero, agenda->mail, agenda->notas);
+        agenda = agenda->sig;
+    }
+    fclose(archivo);
+    printf("Agenda guardada en archivo\n");
+}
+
+contacto* cargar_anterior() {
+    FILE *archivo = fopen("C:\\Users\\sechaniz.ETRR\\Desktop\\OTROS\\ContactosSantu.txt", "r");
+    if (!archivo) {
+        // estaba vació, así que null
+        return NULL;
+    }
+
+    contacto *agenda = NULL, *nuevo;
+    char linea[MAX];
+
+    while (fgets(linea, MAX, archivo)) {
+        nuevo = malloc(sizeof(contacto));
+        if (!nuevo) {
+            printf("No se pudo obtener memnoria\n");
+            break;
+        }
+
+        strcpy(nuevo->nombre, linea);
+        nuevo->nombre[strcspn(nuevo->nombre, "\n")] = 0;
+
+        if (!fgets(nuevo->numero, MAX, archivo)) break;
+        nuevo->numero[strcspn(nuevo->numero, "\n")] = 0;
+
+        if (!fgets(nuevo->mail, MAX, archivo)) break;
+        nuevo->mail[strcspn(nuevo->mail, "\n")] = 0;
+
+        if (!fgets(nuevo->notas, MAX, archivo)) break;
+        nuevo->notas[strcspn(nuevo->notas, "\n")] = 0;
+
+        nuevo->sig = agenda;
+        agenda = nuevo;
+    }
+
+    fclose(archivo);
+    return agenda;
+}
+
+
+void liberar_memoria(contacto* agenda) {
+    contacto* aux;
+    while (agenda) {
+        aux = agenda;
+        agenda = agenda->sig;
+        free(aux);
+    }
+}
---
+++ CORRECTO/vgdhsj.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 50
+
+typedef struct tarea {
+    char materia[MAX];
+    char descripcion[MAX];
+    struct tarea *next;
+} tarea;
+
+typedef enum{
+    AGREGAR=1,
+    MOSTRAR,
+    HECHO,
+    SALIR,
+    BORRAR,
+}opciones_t;
+
+int ingreso(void);
+tarea *tareasGuardadas();
+tarea *agregar_tarea(tarea *lista);
+void imprimiryguardar(tarea *lista);
+void tareahecha(tarea **lista);
+void liberar(tarea *lista);
+
+int main() {
+    tarea *milista = tareasGuardadas();
+    int opcion;
+    do {
+        opcion = ingreso();
+
+        switch (opcion) {
+            case AGREGAR:
+                milista = agregar_tarea(milista);
+            break;
+                
+            case MOSTRAR:
+                imprimiryguardar(milista); 
+            break;
+        
+            case HECHO:
+                tareahecha(&milista);
+            break;
+
+            case SALIR:
+                liberar(milista);
+                printf("chau\n");
+            break;
+            
+            case BORRAR:
+            
+
+            break;
+            
+            default:
+            printf("Opción invalida\n");
+        }
+        
+    }while (opcion != SALIR);
+    return 0;
+}
+
+tarea *tareasGuardadas() {
+    FILE *archivo = fopen("C:\\Users\\sechaniz.ETRR\\Desktop\\VCSCODE EN C\\tareas.txt", "r");
+    if (!archivo){
+        return NULL;
+    }
+    tarea *lista = NULL;
+    char materia[MAX];
+    char descripcion[MAX];
+
+    while (fgets(materia, MAX, archivo) && fgets(descripcion, MAX, archivo)) {
+        materia[scanf(materia, "\n")] = 0;
+        descripcion[scanf(descripcion, "\n")] = 0;
+        tarea *nueva = malloc(sizeof(tarea));
+        if (!nueva){
+        break;
+        }
+        strcpy(nueva->materia, materia);
+        strcpy(nueva->descripcion, descripcion);
+        nueva->next = lista;
+        lista = nueva;
+    }
+    fclose(archivo);
+    return lista;
+}
+
+int ingreso() {
+    int opcion;
+    printf("\nMenu\n");
+    printf("1. Agregar tarea\n");
+    printf("2. Ver tareas\n");
+    printf("3. Tarea hecha\n");
+    printf("4. Salir\n");
+    printf("Elegi una opción: ");
+    scanf("%d", &opcion);
+    getchar();
+    return opcion;
+}
+
+tarea *agregar_tarea(tarea *lista){
+    tarea *nueva = (tarea *)malloc(sizeof(tarea));
+    if (nueva == NULL) {
+        printf("no se pudo reservar memoria, error\n");
+        return lista;
+    }
+    printf("De que materia es esa tarea/pendiente: ");
+    fgets(nueva->materia, MAX, stdin);
+    nueva->materia[strcspn(nueva->materia, "\n")] = 0;
+    printf("Ingresá la descripción de la tarea/pendiente: ");
+    fgets(nueva->descripcion, MAX, stdin);
+    nueva->descripcion[strcspn(nueva->descripcion, "\n")] = 0;
+    nueva->next = lista;
+    printf("Agregado!\n");
+    return nueva;
+}
+
+void imprimiryguardar(tarea *lista){
+    FILE *archivo = fopen("C:\\Users\\sechaniz.ETRR\\Desktop\\VCSCODE EN C\\tareas.txt", "w");
+    if (archivo == NULL) {
+        printf("No se pudo crear o abrir el archivo\n");
+        return;
+    }
+
+    if (lista == NULL) {
+        printf("No hay tareas/pendientes\n");
+        fprintf(archivo, "No hay tareas/pendientes\n");
+        fclose(archivo);
+        return;
+    }
+
+    while (lista) {
+        printf("- %s: %s\n", lista->materia, lista->descripcion);
+        fprintf(archivo, "%s\n%s\n", lista->materia, lista->descripcion);
+        lista = lista->next;
+    }
+
+    fclose(archivo);
+    printf("\nLas tareas/pendientes se guardaron\n");
+
+}
+
+void tareahecha(tarea **lista) {
+    char materia_buscada[MAX];
+    printf("Nombre de la materia de la tarea hecha: ");
+    getchar(); // para limpiar el \n que haya quedado
+    fgets(materia_buscada, MAX, stdin);
+    materia_buscada[strcspn(materia_buscada, "\n")] = 0;
+    tarea *actual = *lista;
+    tarea *anterior = NULL;
+    while (actual != NULL) {
+        if (strcmp(actual->materia, materia_buscada) == 0) {
+            printf("Tarea de '%s' hecha!\n", actual->materia);
+            if (anterior == NULL) {
+                *lista = actual->next;
+            } else {
+                anterior->next = actual->next;
+                free(actual);
+                return;
+            }
+            anterior = actual;
+            actual = actual->next;
+        }
+        printf("No tenés materias con ese nombre\n");
+    }
+}
+
+void liberar(tarea *lista){
+    tarea *aux;
+    while (lista != NULL) {
+        aux = lista;
+        lista = lista->next;
+        free(aux);
+    }
+}
